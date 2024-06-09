5ca9715e389b706926a4e1584110f379cb4e2b2f
“[idrissmortadi]” <idrissmortadi@gmail.com>
2024-06-09 21:35:51 +0200

    Fix Lazyvim

--- init.lua
+++ init.lua
@@ -13,5 +13,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup('plugins')
+require("lazy").setup({
+  spec = 'plugins',  -- Load your plugins
+})
+
 require("vim-options")
